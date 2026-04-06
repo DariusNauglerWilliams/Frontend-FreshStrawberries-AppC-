9e83095ae809c1870699841c713a2e4feb0a673b
W0468712 <W0468712@nscc.ca>
2026-04-06 18:39:55 -0300

    load reviews

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,10 @@ function App() {
   const [data, setData] = useState([]); //set empty array for data
   const [error, setError] = useState(null); //set error state to null for error handling
   const API_URL ="https://localhost:7195/api/movies"; //gets api url  
+const [reviews, setReviews] = useState([]);
+const REV_API_URL = "https://localhost:7195/api/reviews/"; //gets review api url, needs movie id to get reviews for specific movie
+
+
 
 useEffect(() => { //runs when component loads
 
@@ -12,12 +16,23 @@ useEffect(() => { //runs when component loads
 
     try {
       const response = await fetch(API_URL);
+       const reviewResponse = await fetch(REV_API_URL); //fetch reviews
 
       if (!response.ok) { //if response is not ok, throw error
         throw new Error(`HTTP error! status: ${response.status}`);
       }
+
+
       const result = await response.json();
       setData(result); //set data to result
+
+
+    
+      const reviewResult = await reviewResponse.json();
+      setReviews(reviewResult); //set reviews to review result
+      
+
+
 
     } catch (err) {
       setError(err.message); //set error to error message
@@ -28,6 +43,7 @@ useEffect(() => { //runs when component loads
   };fetchData(); //call fetch data function
 
 }, []);
+
 
 
 
@@ -44,7 +60,12 @@ useEffect(() => { //runs when component loads
       <div key={movie.id}>
         <h2>{movie.title}</h2>
         <img src={movie.image} width="200" />
+        
       </div>
+    ))}
+
+    {reviews.map((review) => (
+      <p key={review.id}>{review.createdBy}</p>
     ))}
 
 
