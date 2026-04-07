33c266afe53e98c7bf3e69ce2df11a039eaed8ce
W0468712 <W0468712@nscc.ca>
2026-04-07 13:48:05 -0300

    display init

--- frontend/src/Details.tsx
+++ frontend/src/Details.tsx
@@ -1,13 +1,33 @@
 
 import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Details() {
   const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    if (!id) return;
+
+    fetch(`https://localhost:7195/api/movies/${id}`)
+      .then((response) => response.json())
+      .then((result) => setMovie(result));
+  }, []);
+
   return (
     <>
 
 
-      <h2>Movie Details: {id}</h2>
+      <h2>Movie Details</h2>
+      <div>
+        <p><strong>Title:</strong> {movie.title}</p>
+        <p><strong>ID:</strong> {movie.id}</p>
+        <p><strong>Description:</strong> {movie.description}</p>
+      </div>
+
+
+
+
       <Link to="/">
         <button>Go Back</button>
       </Link>
